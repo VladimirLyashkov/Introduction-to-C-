5121deca5ca5dc01d397d8e42c725e470d37cccb
Vladimir Lyashkov <vladimir.lyashkov14@yandex.ru>
2021-02-02 22:29:41 +0900

    ДЗ к 4 заданию

---
+++ Lesson-4/Lesson-4 (1)/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Lesson_4__1_
+{
+    class Program
+    {
+        /* Написать метод GetFullName(string firstName, string lastName, string patronymic),
+         * принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО.
+         * Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+        */
+
+        static void Main(string[] args)
+        {
+            (string lastName, string firstName, string patronymic)[] users = CreateUsers();
+            PrintUsers(users);
+        }
+
+        static (string firstName, string lastName, string patronymic)[] CreateUsers()
+        {
+            Console.WriteLine("Введите количество пользователей:");
+            int count = ReadInt();
+            (string firstName, string lastName, string patronymic)[] users = new (string firstName, string lastName, string patronymic)[count];
+            for (int i = 0; i < users.Length; i++)
+            {
+                users[i] = GetFullName();
+                Console.WriteLine($"Пользователь {FormatUserData(users[i])} сохранен");
+            }
+
+            Console.WriteLine("Ввод данных завершён. Нажмите любую клавишу...");
+            Console.ReadKey();
+            Console.Clear();
+
+            return users;
+        }
+
+
+        static (string lastName, string firstName, string patronymic) GetFullName()
+        {
+            Console.Write("Введите Фамилию пользователя: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Введите Имя пользователя: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Введите Отчество пользователя: ");
+            string patronymic = Console.ReadLine();
+            return (lastName, firstName, patronymic);
+        }
+
+
+        /// <summary>
+        /// Метод для вывода всего массива в консоль
+        /// </summary>
+        /// <param name="users"></param>
+        static void PrintUsers((string firstName, string lastName, string patronymic)[] users)
+        {
+            Console.WriteLine("Вывод базы данных:");
+            for (int i = 0; i < users.Length; i++)
+            {
+                PrintUser(users[i]);
+            }
+        }
+
+        /// <summary>
+        /// Метод отображения в консоли
+        /// </summary>
+        /// <param name="user"></param>
+        static void PrintUser((string firstName, string lastName, string patronymic) user)
+        {
+            Console.WriteLine(FormatUserData(user));
+        }
+
+        /// <summary>
+        /// Формат вывода текста
+        /// </summary>
+        /// <param name="user">массив</param>
+        /// <returns></returns>
+        static string FormatUserData((string firstName, string lastName, string patronymic) user)
+        {
+            return $"{user.firstName} {user.lastName} {user.patronymic}";
+        }
+
+        /// <summary>
+        /// конвертируем строку в инт
+        /// </summary>
+        /// <returns></returns>
+        static int ReadInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
---
+++ Lesson-4/Lesson-4 (2)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson_4__2_
+{
+    class Program
+    {
+        /*Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом,
+         * и возвращающую число — сумму всех чисел в строке. 
+         * Ввести данные с клавиатуры и вывести результат на экран.
+        */
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите набор чисел через пробел которые хотите просуммировать: ");
+            int sum = ReadInt();
+
+            Console.WriteLine($"Сумма чисел равна: {sum}");
+        }
+                
+        static int ReadInt()
+        {
+            int sum = 0;
+            string setNum = Console.ReadLine();
+            int[] numArr = setNum.Split(" ").Select(int.Parse).ToArray();
+            
+            for (int i = 0; i < numArr.Length; i++)
+            {                
+               sum += numArr[i]; 
+            }
+            return sum;
+        }
+    }
+}
